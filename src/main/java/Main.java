178dd38005444fe25bf9a93d25ff609f5fbac5da
roman <aboandrus@gmail.com>
2019-09-12 21:07:53 +0200

    Implemented code

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,22 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public class Main {
 
     public static int[] sortByValueAndIndex(int[] array) {
+        List<Integer> alist = new ArrayList<Integer>();
+        for(int i =0; i<array.length; i++){
+            alist.add( array[i] * (i+1));
+        }
+        Collections.sort(alist);
+        return alist.stream().mapToInt(i -> i).toArray();
+    }
 
-        return array;
-
+    public static void main (String args[]){
+        int[] array = {3,50,9,8,5};
+        sortByValueAndIndex( array);
     }
 
 }
