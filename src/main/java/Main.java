871154d46a54319377694f2776e91371d5fbe337
roman <aboandrus@gmail.com>
2019-09-12 21:45:50 +0200

    Refactor code

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,16 +2,6 @@ import java.util.*;
 
 public class Main {
 
-//    public static int[] sortByValueAndIndex(int[] array) {
-//        List<Integer> alist = new ArrayList<Integer>();
-//        for(int i =0; i<array.length; i++){
-//            alist.add( array[i] * (i+1));
-//        }
-//        Collections.sort(alist);
-//        return alist.stream().mapToInt(i -> i).toArray();
-//    }
-
-
     public static int[] sortByValueAndIndex(int[] array) {
 
         Map<Integer, Integer> treemap = new TreeMap<>();
@@ -25,6 +15,6 @@ public class Main {
         }
         return clist.stream().mapToInt(i -> (int) i).toArray();
     }
-    
+
 
 }
