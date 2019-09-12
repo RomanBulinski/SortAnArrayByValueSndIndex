7884f57784bcec07e365f2b73236cbfe856ee253
roman <aboandrus@gmail.com>
2019-09-12 21:44:55 +0200

    Improve code against requirements

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,22 +1,30 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Main {
 
-    public static int[] sortByValueAndIndex(int[] array) {
-        List<Integer> alist = new ArrayList<Integer>();
-        for(int i =0; i<array.length; i++){
-            alist.add( array[i] * (i+1));
-        }
-        Collections.sort(alist);
-        return alist.stream().mapToInt(i -> i).toArray();
-    }
+//    public static int[] sortByValueAndIndex(int[] array) {
+//        List<Integer> alist = new ArrayList<Integer>();
+//        for(int i =0; i<array.length; i++){
+//            alist.add( array[i] * (i+1));
+//        }
+//        Collections.sort(alist);
+//        return alist.stream().mapToInt(i -> i).toArray();
+//    }
 
-    public static void main (String args[]){
-        int[] array = {3,50,9,8,5};
-        sortByValueAndIndex( array);
+
+    public static int[] sortByValueAndIndex(int[] array) {
+
+        Map<Integer, Integer> treemap = new TreeMap<>();
+        for(int i =0; i<array.length; i++){
+            treemap.put( array[i] * (i+1), array[i] );
+        }
+        Object[] a = treemap.keySet().toArray();
+        List<Object> clist = new ArrayList<>();
+        for(int i = 0; i< a.length; i++){
+            clist.add( treemap.get(a[i]) );
+        }
+        return clist.stream().mapToInt(i -> (int) i).toArray();
     }
+    
 
 }
